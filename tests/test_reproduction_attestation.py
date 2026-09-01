4524cffc1c873a1f6159c651e5b6024114e5f7e0
jwya <1817219421@qq.com>
2026-09-01 23:36:46 +0800

    style: normalize reproduction attestation import spacing

--- tests/test_reproduction_attestation.py
+++ tests/test_reproduction_attestation.py
@@ -25,7 +25,6 @@ from veritas.reproduction_attestation import (
 )
 from veritas.types import EvidenceGrade, Materiality
 
-
 EMPTY_SHA256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
 
