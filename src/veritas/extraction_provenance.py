c08d26115cd81415027c48636881ab08fa359e7d
jwya <1817219421@qq.com>
2026-09-05 03:01:33 +0800

    feat: expose precommitted external trust policy

--- src/veritas/extraction_provenance.py
+++ src/veritas/extraction_provenance.py
@@ -29,6 +29,17 @@ from .extraction_external_provenance_json import (
     load_extraction_external_trust_root,
     load_extraction_signed_external_provenance,
 )
+from .extraction_external_trust_policy import (
+    ExtractionExternalTrustPolicy,
+    PrecommittedExternalExtractionRunReceipt,
+    build_extraction_external_trust_policy,
+    extraction_external_trust_policy_payload,
+    verify_precommitted_external_extraction_provenance_for_run,
+)
+from .extraction_external_trust_policy_json import (
+    extraction_external_trust_policy_json_payload,
+    load_extraction_external_trust_policy,
+)
 
 __all__ = [
     "AttestedExtractionEvidenceReleaseReceipt",
@@ -38,18 +49,25 @@ __all__ = [
     "ExtractionExecutionEvidence",
     "ExtractionExecutionPlan",
     "ExtractionExternalProvenanceStatement",
+    "ExtractionExternalTrustPolicy",
     "ExtractionExternalTrustRoot",
     "ExtractionSignedExternalProvenance",
+    "PrecommittedExternalExtractionRunReceipt",
     "build_attested_extraction_evidence_release_receipt",
     "build_extraction_execution_evidence",
     "build_extraction_external_provenance_statement",
+    "build_extraction_external_trust_policy",
     "extraction_external_provenance_statement_bytes",
+    "extraction_external_trust_policy_json_payload",
+    "extraction_external_trust_policy_payload",
     "extraction_external_trust_root_payload",
     "extraction_prediction_artifact_bytes",
     "extraction_prediction_semantics_sha256",
     "extraction_signed_external_provenance_payload",
+    "load_extraction_external_trust_policy",
     "load_extraction_external_trust_root",
     "load_extraction_signed_external_provenance",
     "verify_external_extraction_provenance",
     "verify_external_extraction_provenance_for_run",
+    "verify_precommitted_external_extraction_provenance_for_run",
 ]
